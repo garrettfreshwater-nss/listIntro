c95817b56133ba784c25397200cf50e5f7d9773c
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-02-28 09:45:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIntro {
+    class Program {
+        static void Main (string[] args) {
+            List<string> nssCohorts = new List<string> {
+                "Cohort 36",
+                "Cohort 37",
+                "Cohort 38",
+                "Cohort 39"
+            };
+
+            List<int> birthYears = new List<int> {
+                1996,
+                1992,
+                1997,
+                2001,
+            };
+
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
